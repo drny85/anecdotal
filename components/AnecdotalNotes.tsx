b74d9282479fc3975bed3b5f5920f31088c17b88
Robert Melendez <melendez@robertdev.net>
2023-11-01 11:46:34 -0400

    change chevron icon

--- components/AnecdotalNotes.tsx
+++ components/AnecdotalNotes.tsx
@@ -1,8 +1,9 @@
 import { FieldValues } from "@/types";
 import * as Accordion from "@radix-ui/react-accordion";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
-import { Box, Flex, Text, TextArea } from "@radix-ui/themes";
+import { Box, Card, Flex, Text, TextArea } from "@radix-ui/themes";
 import React, { useEffect, useState } from "react";
+import { BsChevronDown } from "react-icons/bs";
 
 type Props = {
   fields: FieldValues[];
@@ -39,33 +40,37 @@ const AnecdotalNotes = ({ fields }: Props) => {
   return (
     <Box>
       {state.map((c) => (
-        <Flex key={c.id} direction={"column"} gap={"5"} my="6">
-          <Accordion.Root type="single" collapsible>
-            <Accordion.Item className="AccordionItem" value={c.value}>
-              <Accordion.Trigger className="text-xl font-semibold flex items-center justify-between w-full">
-                {c.id} - {c.value}
-                <ChevronDownIcon
+        <Flex key={c.id} direction={"column"} gap={"5"} my="5">
+          <Card>
+            <Accordion.Root type="single" collapsible>
+              <Accordion.Item className="AccordionItem" value={c.value}>
+                <Accordion.Trigger className="text-xl font-semibold flex items-center justify-between w-full">
+                  {c.id} - {c.value}
+                  {/* <ChevronDownIcon
                   className="AccordionChevron"
                   aria-hidden
                   fontSize={36}
-                />
-              </Accordion.Trigger>
-              <Accordion.Content>
-                {c.subvalue.map((f, i) => (
-                  <Box key={f.name} my={"3"} p={"2"}>
-                    <Text className="font-semibold">
-                      {i + 1}- {f.name}
-                    </Text>
-                    <TextArea
-                      placeholder={`Write ${f.name}`}
-                      value={f.value}
-                      onChange={(e) => onChange(e, f.name)}
-                    />
-                  </Box>
-                ))}
-              </Accordion.Content>
-            </Accordion.Item>
-          </Accordion.Root>
+                  fontWeight={"bold"}
+                /> */}
+                  <BsChevronDown className="AccordionChevron font-bold h-6 w-8" />
+                </Accordion.Trigger>
+                <Accordion.Content>
+                  {c.subvalue.map((f, i) => (
+                    <Box key={f.name} my={"3"} p={"2"}>
+                      <Text className="font-semibold">
+                        {i + 1}- {f.name}
+                      </Text>
+                      <TextArea
+                        placeholder={`Write ${f.name}`}
+                        value={f.value}
+                        onChange={(e) => onChange(e, f.name)}
+                      />
+                    </Box>
+                  ))}
+                </Accordion.Content>
+              </Accordion.Item>
+            </Accordion.Root>
+          </Card>
         </Flex>
       ))}
     </Box>
